b4ed56318b8cbeada205f1dce56e5c513b68172e
bauticarbo <capocarbo@gmail.com>
2022-09-08 19:37:41 -0300

    incorporacion de toastify

--- main.js
+++ main.js
@@ -1,12 +1,6 @@
 // aca empieza
 
 let carrito = [];
-let carritoEnLS = JSON.parse(localStorage.getItem('carritoJSON'));
-/*if (carritoEnLS) {
-  carrito = carritoEnLS;
-}*/
-
-let enCarrito;
 
 let totalCarrito = 0;
 
@@ -41,16 +35,23 @@ function sumarAlCarrito(id) {
       carrito.push(productosStock[5]);
       totalCarrito += productosStock[5].precio;
       break;
+    case 106:
+      carrito.push(productosStock[6]);
+      totalCarrito += productosStock[6].precio;
+      break;
   }
 
   carritoFinal.innerHTML = `<p>Los productos que colocó en el carrito son: </p>`;
-
   for (let i = 0; i < carrito.length; i++) {
     carritoFinal.innerHTML += `<div>- ${carrito[i].prod}  $${carrito[i].precio} <button class="btn-eliminar"  onclick="">Eliminar del carrito</button></div>`;
   }
 
   carritoFinal.innerHTML += `<p> Sumando un total de $${totalCarrito}</p> <div><button class="btn-finalizar" id="btn-finalizar" onclick="">FINALIZAR COMPRA</button></div>`;
-  let carritoJSON = JSON.stringify(carrito);
+
+  Toastify({
+    text: 'Producto agregado!',
+    duration: 2000,
+  }).showToast();
 }
 
 const productosStock = [
@@ -60,16 +61,21 @@ const productosStock = [
   { prod: 'buzo', precio: 4000, id: 103 },
   { prod: 'campera de jean', precio: 7000, id: 104 },
   { prod: 'zapatillas', precio: 11000, id: 105 },
+  { prod: 'botines', precio: 15000, id: 106 },
 ];
 
 let productosHTML;
 
 for (const item of productosStock) {
-  productosHTML += `<div class="prod"><p>Producto: ${item.prod} <br>Precio: $${item.precio}</p><button id="btn${item.id}" onclick="sumarAlCarrito(${item.id})">Agregar al carrito</button></div>`;
+  productosHTML += `<div class="prod"><p>Producto: ${item.prod} <br>Precio: $${item.precio}</p><button class="btn-prod" id="btn${item.id}" onclick="sumarAlCarrito(${item.id})">Agregar al carrito</button></div>`;
 }
 
+/*document.getElementsByClassName('btn-prod').onclick(
+  Toastify({
+    text: 'Probando toast!',
+    duration: 3000,
+  }).showToast()
+);*/
+
 document.getElementById('productos').innerHTML = productosHTML;
-
-const carritoJSON = JSON.stringify(carrito);
-
-localStorage.setItem('carritoJSON', carritoJSON);
+console.log(carrito);
